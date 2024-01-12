e5152c14a1b246262f41650ff122437752b028b4
mkc-c <abdirisak.arr@gmail.com>
2024-01-12 07:04:06 +0000

    make bill input component controlled, accepts only numbers as values

--- src/components/BillInput.js
+++ src/components/BillInput.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function BillInput({ children }) {
+  const [bill, setBill] = useState(0);
+
+  const handleChange = (event) => {
+    const inputValue = Number(event.target.value);
+    if (isNaN(inputValue)) return;
+    setBill(inputValue);
+  };
+
   return (
     <div>
       <label htmlFor="bill">{children}</label>
-      <input id="bill" type="text" />
+      <input id="bill" type="text" value={bill} onChange={handleChange} />
     </div>
   );
 }
