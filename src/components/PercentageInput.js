354a2d931bdb872d2cc0a6482214869b0620c683
mkc-c <abdirisak.arr@gmail.com>
2024-01-12 07:37:04 +0000

    make percentage input component reusable and controlled, create rendered options list from prop object passed down

--- src/components/PercentageInput.js
+++ src/components/PercentageInput.js
@@ -1,5 +1,28 @@
-function PercentageInput() {
-  return <div>PercentageInput</div>;
+import { useState } from "react";
+
+function PercentageInput({ children, options }) {
+  const [percentage, setPercentage] = useState(options[0].value);
+
+  const handleChange = (event) => {
+    setPercentage(event.target.value);
+  };
+
+  const renderedOptions = options.map((option, index) => {
+    return (
+      <option key={index} value={option.value}>
+        {option.label}
+      </option>
+    );
+  });
+
+  return (
+    <div>
+      <label>{children}</label>
+      <select value={percentage} onChange={handleChange}>
+        {renderedOptions}
+      </select>
+    </div>
+  );
 }
 
 export default PercentageInput;
