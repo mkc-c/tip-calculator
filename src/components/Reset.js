da05e78a900675cfe28bd06fa6310051d62f3290
mkc-c <abdirisak.arr@gmail.com>
2024-01-12 09:42:10 +0000

    write logic for reset component

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,5 +1,10 @@
-function Reset() {
-  return <div>Reset</div>;
+function Reset({ bill, user, friend, onReset }) {
+  if (bill || user || friend)
+    return (
+      <div>
+        <button onClick={onReset}>Reset</button>
+      </div>
+    );
 }
 
 export default Reset;
