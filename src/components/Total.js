a85e2baaca59d1a4b9e821a27649dc14e83d01ea
mkc-c <abdirisak.arr@gmail.com>
2024-01-12 09:41:31 +0000

    write logic and jsx for total component

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,22 @@
-function Total() {
-  return <div>Total</div>;
+function Total({ total, bill, tip }) {
+  let content;
+
+  if (!bill) return null;
+  else if (total && !tip) {
+    content = `You pay $${bill} ($${bill} + $${tip})`;
+  } else {
+    content = `You pay $${bill} ($${total} + $${tip})`;
+  }
+
+  console.log(typeof content);
+
+  return (
+    <div>
+      <h2>
+        <b>{content}</b>
+      </h2>
+    </div>
+  );
 }
 
 export default Total;
